94137112404e91047c079d754f0337c4e5630865
HsShortland <98280528+HsShortland@users.noreply.github.com>
2022-12-06 14:33:25 +0000

    Added hooks and useState

--- src/Components/Button1.js
+++ src/Components/Button1.js
@@ -1,10 +1,32 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
 import  FontAwesome  from '@expo/vector-icons/FontAwesome';
+import React, { useEffect, useState } from 'react';
 
-export default function Button1() {
-    
+export default function Button1(props) {
+    const [generateWord, setGenerateWord] = useState('Click Here');
+    const [count, setCount] = useState(0);
+
+    useEffect(()=>{
+        console.log('Button1 successfully mounted')
+    }, [])
+
+    useEffect(()=>{
+        console.log(`Count was updated to ${count}`)
+    }, [count])
+
+    function changeButtonText(){
+        var words = ["Press the button!", "Tap the button", "Touch the button"]
+        var word = words[Math.floor(Math.random()*words.length)]
+        setGenerateWord(word)
+        console.log("Random word is: " + word)
+    }
+
+    function counter(){
+        setCount(count+1)
+    }
+
     return (
-            <FontAwesome.Button title='Click Me!' backgroundColor={'#63a4ff'} name="random"> Click Here!
+            <FontAwesome.Button  backgroundColor={'#63a4ff'} name="random" onPress={()=>{changeButtonText(); counter();}}>{generateWord}
             </FontAwesome.Button>
 );
 }
