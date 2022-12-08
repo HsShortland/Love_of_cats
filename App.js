8e9e9ac719caeeb44fcbf305b34c3d1a5ce21950
HsShortland <98280528+HsShortland@users.noreply.github.com>
2022-12-08 01:08:47 +0000

    redux ->global state of add/remove user +FA fonts

--- App.js
+++ App.js
@@ -5,11 +5,34 @@ import FilmsWithCats from './src/Screens/FilmsWithCats';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import  FontAwesome  from '@expo/vector-icons/FontAwesome';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import FontAwesomeIcons from './src/Components/FontAwesomeIcons';
 
 const Tab = createBottomTabNavigator();
 
+function userReducer(state, action){
+  switch(action.type){
+    case "ADD_USER":
+      console.log('here')
+      return state.username = {...state, username: action.payload}
+    case "REMOVE_USER":
+      console.log('remove')
+      return state.username = {...state, username:"None"}
+    default:
+      console.log('default')
+      return state
+  }
+}
+
+const initialState = {username:'No user set yet'}
+
+const store = createStore(userReducer, initialState)
+
 const App = () =>{
-  return(
+
+return(
+    <Provider store={store}>
     <NavigationContainer>
     <Tab.Navigator
       screenOptions={({ route }) => ({tabBarIcon: () => {
@@ -20,8 +43,7 @@ const App = () =>{
               return <FontAwesome 
             name={'info'}/>;
             } else if (route.name === 'CuteCats') {
-              return <FontAwesome 
-              name={'photo'}/>;
+              return <FontAwesomeIcons></FontAwesomeIcons>;
             }
             else if (route.name === 'FilmsWithCats') {
               return <FontAwesome 
@@ -33,12 +55,13 @@ const App = () =>{
           tabBarInactiveBackgroundColor: '#eeffff',
           tabBarActiveBackgroundColor: '#8aacc8'
         })}>
-    <Tab.Screen name="Home"component={HomeScreen} oprttions={{title: 'Welcome'}}/>
+    <Tab.Screen name="Home"component={HomeScreen} options={{title: 'Welcome'}}/>
     <Tab.Screen name = "AboutTheDev" component ={AboutTheDev}/>
     <Tab.Screen name = "CuteCats" component ={CuteCats}/>
     <Tab.Screen name = "FilmsWithCats" component ={FilmsWithCats}/>
     </Tab.Navigator>
 </NavigationContainer>
+</Provider>
   );
 };
 
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -4,14 +4,43 @@ import Button1 from '../Components/Button1';
 import Button2 from '../Components/Button2';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { useSelector, useDispatch } from 'react-redux';
+import FontAwesomeIcons from '../Components/FontAwesomeIcons';
 
 export default function HomeScreen({navigation}) {
+
+const user = useSelector((state) => state.username)
+
+const dispatch = useDispatch()
+
+function addUser(user){
+    const action = {
+        type: "ADD_USER",
+        payload: user
+    }
+    dispatch(action)
+}
+
+function removeUser(user){
+    const action = {
+        type: "REMOVE_USER",
+        payload: user
+    }
+    dispatch(action)
+}
+
     return (
+
+        
     <View style={styles.container}>
         <Text style={styles.text}>Welcome to my app all about cats!</Text>
         <StatusBar style="auto" />
+        <FontAwesomeIcons></FontAwesomeIcons>
         <Text style={styles.text}>Cats are totally amazing!</Text>
         <View style={styles.buttons}>
+            <Text>User: {user}</Text>
+            <Button title={"ADD_USER"} onPress={() =>addUser("Helen")}></Button>
+            <Button title={"REMOVE_USER"} onPress={()=>removeUser()}></Button>
             <Button1></Button1>
             <Button2 details='Click Here'></Button2>
         </View>
